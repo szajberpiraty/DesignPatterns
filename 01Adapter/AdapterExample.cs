a0fe7c6bc8aa20f69bde1c3c3c493a77b35103f9
szajberpiraty <szajberpiraty@gmail.com>
2018-10-18 20:07:32 +0200

    Design Patterns

---
+++ 01Adapter/AdapterExample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01Adapter
+{
+    public class AdapterExample
+    {
+        public void Start()
+        {
+            //legyen egy adatforrásunk és egy email megoldásunk és ezeket kössük össze
+            //folytatás a 39 perctől
+        }
+    }
+}
--- 01Adapter/Program.cs
+++ 01Adapter/Program.cs
@@ -8,8 +8,17 @@ namespace _01Adapter
 {
     class Program
     {
+        /// <summary>
+        /// Feladat:körüzenet küldése ügyfeleknek
+        /// Ehhez majd lesz adatbázis, illetve valamilyen üzenetküldő szolgáltatás, de 
+        /// egyelőre egyik sincsen meg, de  később illeszkedni kell hozzá
+        /// </summary>
+        /// <param name="args"></param>
+
         static void Main(string[] args)
         {
+            var example = new AdapterExample();
+            example.Start();
         }
     }
 }
